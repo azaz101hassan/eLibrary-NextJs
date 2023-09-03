dd2fcf5f0fd4a5be0eb750ffac70ca08a27baa2e
azaz101hassan <azaz.hassan.ts@gmail.com>
2023-09-03 12:01:44 +0500

    Initial Commit + Skeleton

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
