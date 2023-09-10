edbfb3b7269f7a5f858ac8b76c02672ca0173a46
azaz101hassan <azaz101hassan@gmail.com>
2023-09-10 14:32:06 +0500

    Add Dark Mode + Dynamic Book Details Page

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { HiSun, HiMoon } from "react-icons/hi";
 
 const Header = () => {
-  return (
-    <div className='border-b-2'>
-    <h1 className='text-center text-lg m-7 ml-0 mr-0'>E-Dynamic Library</h1>
-    </div>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-export default Header
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <header>
+      <div className="border-b-2">
+        <div
+          className="px-4 py-4 flex items-center"
+          style={{ marginRight: `3.5rem` }}
+        >
+          <h1 className="ml-10 text-lg">
+            E-Dynamic Library
+          </h1>
+          <div className="ml-auto">
+            {mounted &&
+              (theme === "dark" ? (
+                <HiSun
+                  className="w-10 h-10 text-yellow-500"
+                  role="button"
+                  onClick={() => setTheme("light")}
+                />
+              ) : (
+                <HiMoon
+                  className="w-10 h-10 text-gray-900"
+                  role="button"
+                  onClick={() => setTheme("dark")}
+                />
+              ))}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
